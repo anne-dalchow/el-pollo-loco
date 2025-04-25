a93d47deeb116b270512002e0c8e7c598ae7f77e
Anne Dalchow <anne.dalchow@web.de>
2025-04-25 07:06:08 +0200

    add animate() function

--- modules/Chicken.class.js
+++ modules/Chicken.class.js
@@ -1,16 +1,16 @@
 import MoveableObject from "./MoveableObject.class.js";
 
 export default class Chicken extends MoveableObject {
+  posY = 350;
+  width = 80;
+  height = 80;
+
   constructor() {
     super();
     this.loadImg("assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.posX = 250 + Math.random() * 500;
-    this.posY = 350;
-    this.width = 80;
-    this.height = 80;
+    this.animate(1);
   }
 
-  eat() {
-    console.log("chicken eat");
-  }
+
 }
--- modules/MoveableObject.class.js
+++ modules/MoveableObject.class.js
@@ -1,13 +1,39 @@
 export default class MoveableObject {
   posX;
   posY;
-  img;
+
   height;
   width;
+  img;
+  imageCache = {};
 
   loadImg(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+  /**
+   * 
+   * @param {Array} arr - ['img/image1.png','img/image1.png',....] Load the image array, create a new Image object, assign the image path to its src property, and add it to the imageCache. Each image is loaded by creating a new Image object, setting its src to the corresponding path, and storing it in the imageCache for later use.
+   */
+
+  loadImages(arr){
+    arr.forEach((path)=>{
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = path;
+    })
+
+  }
+
+  animate(posX){
+    setInterval(() => {   
+      if(this.posX > -350){
+        this.posX -=posX;
+      } else{
+        this.posX = 800;
+      }
+    },1000/ 60);
   }
 
   moveRight() {
