0768e9b56ca18c47451deee0e00cf5e84f68977c
Anne Dalchow <anne.dalchow@web.de>
2025-04-28 16:53:33 +0200

    Create BottleBar.class.js

---
+++ modules/BottleBar.class.js
@@ -0,0 +1,46 @@
+import DrawableObject from "./DrawableObject.class.js";
+
+export default class BottleBar extends DrawableObject {
+  IMAGES_BOTTLE = [
+    "assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png",
+    "assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png",
+    "assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png",
+    "assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png",
+    "assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png",
+    "assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png",
+  ];
+
+  percentage = 100;
+  width = 160;
+  height = 50;
+
+  constructor() {
+    super();
+    this.loadImages(this.IMAGES_BOTTLE);
+    this.posX = 180;
+    this.posY = 10;
+    this.setPercentage(100);
+  }
+
+  setPercentage(percentage) {
+    this.percentage = percentage; // => 0 ... 5
+    let path = this.IMAGES_BOTTLE[this.resolveImageIndex()];
+    this.img = this.imageCache[path];
+  }
+
+  resolveImageIndex() {
+    if (this.percentage == 100) {
+      return 5;
+    } else if (this.percentage > 80) {
+      return 4;
+    } else if (this.percentage > 60) {
+      return 3;
+    } else if (this.percentage > 40) {
+      return 2;
+    } else if (this.percentage > 20) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
