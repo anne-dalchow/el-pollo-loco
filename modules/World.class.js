ed9984f90f05398c74e4c153cc29923eafd8b2d8
Anne Dalchow <anne.dalchow@web.de>
2025-04-28 16:56:51 +0200

    udate world class
    add checkCollisions()
    add statusbars

--- modules/World.class.js
+++ modules/World.class.js
@@ -1,8 +1,10 @@
 import Character from "./Character.class.js";
 import { level1 } from "../levels/level1.js";
+import StatusBar from "./StatusBar.class.js";
+import CoinBar from "./CoinBar.class.js";
+import BottleBar from "./BottleBar.class.js";
 
 export default class World {
-
   level = level1;
   canvas;
   ctx;
@@ -13,10 +15,24 @@ export default class World {
     this.canvas = canvas;
     this.keyboard = keyboard; // Steuerung wird gespeichert
     this.character = new Character(this, this.keyboard);
+    this.healthBar = new StatusBar();
+    this.coinBar = new CoinBar();
+    this.bottleBar = new BottleBar();
 
     this.draw(); // startet Loop
+    this.checkCollisions();
   }
 
+  checkCollisions() {
+    setInterval(() => {
+      this.level.enemies.forEach((enemy) => {
+        if (this.character.isColliding(enemy)) {
+          this.character.hit();
+          this.healthBar.setPercentage(this.character.energy);
+        }
+      });
+    }, 200);
+  }
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.character.move();
@@ -24,6 +40,14 @@ export default class World {
     this.ctx.translate(this.camera_x, 0);
 
     this.addObjectsToMap(this.level.backgroundObjects);
+
+    this.ctx.translate(-this.camera_x, 0);
+    // ---------- Space for fixed Objects ----------
+    this.addToMap(this.healthBar);
+    this.addToMap(this.coinBar);
+    this.addToMap(this.bottleBar);
+    this.ctx.translate(this.camera_x, 0);
+
     this.addObjectsToMap(this.level.clouds);
     this.addObjectsToMap(this.level.enemies);
     this.addToMap(this.character);
@@ -43,7 +67,10 @@ export default class World {
     if (mo.otherDirection) {
       this.flipImage(mo);
     }
-    this.ctx.drawImage(mo.img, mo.posX, mo.posY, mo.width, mo.height);
+
+    mo.draw(this.ctx);
+    mo.drawFrame(this.ctx);
+
     if (mo.otherDirection) {
       this.flipImageBack(mo);
     }
