e780c1102cd63fa0e0dc23b4259af0fe329804c5
Anne Dalchow <anne.dalchow@web.de>
2025-04-28 16:54:38 +0200

    update cloud class

--- modules/Clouds.class.js
+++ modules/Clouds.class.js
@@ -9,6 +9,19 @@ export default class Clouds extends MoveableObject {
     super();
     this.loadImg("assets/img/5_background/layers/4_clouds/1.png");
     this.posX = Math.random() * 800;
-    this.moveLeft(0.15,-350);
+    this.moveLeft(0.15, -350);
+  }
+  moveLeft(speed, end) {
+    const animate = () => {
+      if (this.posX > end) {
+        this.posX -= speed;
+      } else {
+        this.posX = 800;
+      }
+
+      /**animate-Funktion: konstante Bewegung ohne zeitliche Verzögerung -> daher ohne Pausen/setTimeout */
+      requestAnimationFrame(animate);
+    };
+    requestAnimationFrame(animate);
   }
 }
