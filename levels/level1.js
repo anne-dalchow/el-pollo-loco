6ac16fb4e4422a035d24268ff7c245b87e546b04
Anne Dalchow <anne.dalchow@web.de>
2025-04-29 11:29:28 +0200

    update level1 spawn enemies
    for dynamic number of enemies

--- levels/level1.js
+++ levels/level1.js
@@ -5,11 +5,23 @@ import { layers } from "./backgroundLevel1.js";
 import Clouds from "../modules/Clouds.class.js";
 import Endboss from "../modules/Endboss.class.js";
 
+const levelWidth = 799 * 6;
+const numberOfChickens = 7;
+const distancePerChicken = levelWidth / numberOfChickens;
+const chickens = [];
+
+for (let i = 1; i <= numberOfChickens; i++) {
+  let basePos = i * distancePerChicken;
+  let randomOffset = (Math.random() - 0.5) * 300; // leichte Abweichung (+/- 100px)
+  let finalPos = basePos + randomOffset;
+
+  let chicken = new Chicken();
+  chicken.posX = finalPos;
+  chickens.push(chicken);
+}
+
 export const level1 = new Level(
- [new Chicken(), new Chicken(), new Chicken(), new Chicken(), new Endboss()],
- [new Clouds(), new Clouds()],
- 
- layers.map(layer => 
-     new BackgroundObject(layer.path, layer.offsetX, 0)
-   )
+  [...chickens, new Endboss()],
+  [new Clouds(), new Clouds()],
+  layers.map((layer) => new BackgroundObject(layer.path, layer.offsetX, 0))
 );
