c0f69f666171f24608963dd8a54529c55a1bc9ae
Anne Dalchow <anne.dalchow@web.de>
2025-04-29 11:28:41 +0200

    update backgroundLevel1

--- levels/backgroundLevel1.js
+++ levels/backgroundLevel1.js
@@ -1,41 +1,89 @@
 export const layers = [
- { path: "assets/img/5_background/layers/air.png", offsetX: -799 },
- { path: "assets/img/5_background/layers/3_third_layer/2.png", offsetX: -799 },
- { path: "assets/img/5_background/layers/2_second_layer/2.png", offsetX: -799 },
- { path: "assets/img/5_background/layers/1_first_layer/2.png", offsetX: -799 },
+  { path: "assets/img/5_background/layers/air.png", offsetX: -799 },
+  { path: "assets/img/5_background/layers/3_third_layer/2.png", offsetX: -799 },
+  {
+    path: "assets/img/5_background/layers/2_second_layer/2.png",
+    offsetX: -799,
+  },
+  { path: "assets/img/5_background/layers/1_first_layer/2.png", offsetX: -799 },
 
- { path: "assets/img/5_background/layers/air.png", offsetX: 0 },
- { path: "assets/img/5_background/layers/3_third_layer/1.png", offsetX: 0 },
- { path: "assets/img/5_background/layers/2_second_layer/1.png", offsetX: 0 },
- { path: "assets/img/5_background/layers/1_first_layer/1.png", offsetX: 0 },
+  { path: "assets/img/5_background/layers/air.png", offsetX: 0 },
+  { path: "assets/img/5_background/layers/3_third_layer/1.png", offsetX: 0 },
+  { path: "assets/img/5_background/layers/2_second_layer/1.png", offsetX: 0 },
+  { path: "assets/img/5_background/layers/1_first_layer/1.png", offsetX: 0 },
 
- { path: "assets/img/5_background/layers/air.png", offsetX: 799 },
- { path: "assets/img/5_background/layers/3_third_layer/2.png", offsetX: 799 },
- { path: "assets/img/5_background/layers/2_second_layer/2.png", offsetX: 799 },
- { path: "assets/img/5_background/layers/1_first_layer/2.png", offsetX: 799 },
+  { path: "assets/img/5_background/layers/air.png", offsetX: 799 },
+  { path: "assets/img/5_background/layers/3_third_layer/2.png", offsetX: 799 },
+  { path: "assets/img/5_background/layers/2_second_layer/2.png", offsetX: 799 },
+  { path: "assets/img/5_background/layers/1_first_layer/2.png", offsetX: 799 },
 
- { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 2 },
- { path: "assets/img/5_background/layers/3_third_layer/1.png", offsetX: 799 * 2 },
- { path: "assets/img/5_background/layers/2_second_layer/1.png", offsetX: 799 * 2 },
- { path: "assets/img/5_background/layers/1_first_layer/1.png", offsetX: 799 * 2 },
+  { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 2 },
+  {
+    path: "assets/img/5_background/layers/3_third_layer/1.png",
+    offsetX: 799 * 2,
+  },
+  {
+    path: "assets/img/5_background/layers/2_second_layer/1.png",
+    offsetX: 799 * 2,
+  },
+  {
+    path: "assets/img/5_background/layers/1_first_layer/1.png",
+    offsetX: 799 * 2,
+  },
 
- { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 3 },
- { path: "assets/img/5_background/layers/3_third_layer/2.png", offsetX: 799 * 3 },
- { path: "assets/img/5_background/layers/2_second_layer/2.png", offsetX: 799 * 3 },
- { path: "assets/img/5_background/layers/1_first_layer/2.png", offsetX: 799 * 3 },
+  { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 3 },
+  {
+    path: "assets/img/5_background/layers/3_third_layer/2.png",
+    offsetX: 799 * 3,
+  },
+  {
+    path: "assets/img/5_background/layers/2_second_layer/2.png",
+    offsetX: 799 * 3,
+  },
+  {
+    path: "assets/img/5_background/layers/1_first_layer/2.png",
+    offsetX: 799 * 3,
+  },
 
- { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 4 },
- { path: "assets/img/5_background/layers/3_third_layer/1.png", offsetX: 799 * 4 },
- { path: "assets/img/5_background/layers/2_second_layer/1.png", offsetX: 799 * 4 },
- { path: "assets/img/5_background/layers/1_first_layer/1.png", offsetX: 799 * 4 },
+  { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 4 },
+  {
+    path: "assets/img/5_background/layers/3_third_layer/1.png",
+    offsetX: 799 * 4,
+  },
+  {
+    path: "assets/img/5_background/layers/2_second_layer/1.png",
+    offsetX: 799 * 4,
+  },
+  {
+    path: "assets/img/5_background/layers/1_first_layer/1.png",
+    offsetX: 799 * 4,
+  },
 
- { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 5 },
- { path: "assets/img/5_background/layers/3_third_layer/2.png", offsetX: 799 * 5 },
- { path: "assets/img/5_background/layers/2_second_layer/2.png", offsetX: 799 * 5 },
- { path: "assets/img/5_background/layers/1_first_layer/2.png", offsetX: 799 * 5 },
- 
- { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 6 },
- { path: "assets/img/5_background/layers/3_third_layer/1.png", offsetX: 799 * 6 },
- { path: "assets/img/5_background/layers/2_second_layer/1.png", offsetX: 799 * 6 },
- { path: "assets/img/5_background/layers/1_first_layer/1.png", offsetX: 799 * 6 },
+  { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 5 },
+  {
+    path: "assets/img/5_background/layers/3_third_layer/2.png",
+    offsetX: 799 * 5,
+  },
+  {
+    path: "assets/img/5_background/layers/2_second_layer/2.png",
+    offsetX: 799 * 5,
+  },
+  {
+    path: "assets/img/5_background/layers/1_first_layer/2.png",
+    offsetX: 799 * 5,
+  },
+
+  { path: "assets/img/5_background/layers/air.png", offsetX: 799 * 6 },
+  {
+    path: "assets/img/5_background/layers/3_third_layer/1.png",
+    offsetX: 799 * 6,
+  },
+  {
+    path: "assets/img/5_background/layers/2_second_layer/1.png",
+    offsetX: 799 * 6,
+  },
+  {
+    path: "assets/img/5_background/layers/1_first_layer/1.png",
+    offsetX: 799 * 6,
+  },
 ];
