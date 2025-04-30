40d74cf6d492ad117d03c1d92c2cfab0a6eed29d
Anne Dalchow <anne.dalchow@web.de>
2025-04-30 16:26:30 +0200

    create coin class, add coins to level1

---
+++ modules/Coins.class.js
@@ -0,0 +1,33 @@
+import MoveableObject from "./MoveableObject.class.js";
+
+export default class Coins extends MoveableObject {
+  IMAGES_COINS = [
+    "assets/img/8_coin/Gold_21.png",
+    "assets/img/8_coin/Gold_22.png",
+    "assets/img/8_coin/Gold_23.png",
+    "assets/img/8_coin/Gold_24.png",
+    "assets/img/8_coin/Gold_25.png",
+    "assets/img/8_coin/Gold_26.png",
+    "assets/img/8_coin/Gold_27.png",
+    "assets/img/8_coin/Gold_28.png",
+    "assets/img/8_coin/Gold_29.png",
+    "assets/img/8_coin/Gold_30.png",
+  ];
+
+  constructor() {
+    super();
+    this.posY = Math.random() * 250;
+    this.posX = 50 + Math.random() * 3000;
+    this.height = 35;
+    this.width = 30;
+    this.loadImg("assets/img/8_coin/Gold_21.png");
+    this.loadImages(this.IMAGES_COINS);
+    this.coinAnimation();
+  }
+
+  coinAnimation() {
+    this.coinIntervall = setInterval(() => {
+      this.playAnimation(this.IMAGES_COINS);
+    }, 1000 / 15);
+  }
+}
