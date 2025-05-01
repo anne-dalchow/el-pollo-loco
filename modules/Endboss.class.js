87d1777e80004b9ac06e2b45a6f035c6f9a467d0
Anne Dalchow <anne.dalchow@web.de>
2025-05-01 13:27:12 +0200

    Update Endboss.class.js

--- modules/Endboss.class.js
+++ modules/Endboss.class.js
@@ -1,37 +1,85 @@
+import {
+  IMAGES_WALKING,
+  IMAGES_ALERT,
+  IMAGES_ATTACK,
+  IMAGES_HURT,
+  IMAGES_DEAD,
+} from "../levels/endbossImages.js";
 import MoveableObject from "./MoveableObject.class.js";
 
 export default class Endboss extends MoveableObject {
   posY = 40;
   width = 300;
   height = 400;
-  IMAGES_WALKING = [
-    "assets/img/4_enemie_boss_chicken/2_alert/G5.png",
-    "assets/img/4_enemie_boss_chicken/2_alert/G6.png",
-    "assets/img/4_enemie_boss_chicken/2_alert/G7.png",
-    "assets/img/4_enemie_boss_chicken/2_alert/G8.png",
-    "assets/img/4_enemie_boss_chicken/2_alert/G9.png",
-    "assets/img/4_enemie_boss_chicken/2_alert/G10.png",
-    "assets/img/4_enemie_boss_chicken/2_alert/G11.png",
-    "assets/img/4_enemie_boss_chicken/2_alert/G12.png",
-  ];
 
   constructor() {
     super();
+    this.IMAGES_WALKING = IMAGES_WALKING;
+    this.IMAGES_ALERT = IMAGES_ALERT;
+    this.IMAGES_ATTACK = IMAGES_ATTACK;
+    this.IMAGES_HURT = IMAGES_HURT;
+    this.IMAGES_DEAD = IMAGES_DEAD;
+
+    this.posX = 799 * 6;
     this.loadImg("assets/img/4_enemie_boss_chicken/2_alert/G5.png");
     this.loadImages(this.IMAGES_WALKING);
-    this.posX = 799 * 6;
+    this.loadImages(this.IMAGES_ALERT);
+    this.loadImages(this.IMAGES_ATTACK);
+    this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_DEAD);
+    this.isDead = false;
 
     this.endbossWalkAnimation();
-    // this.moveLeft(randomSpeed, -60);
   }
 
   endbossWalkAnimation() {
-    const animate = () => {
-      this.playAnimation(this.IMAGES_WALKING);
-      setTimeout(() => {
-        requestAnimationFrame(animate);
-      }, 100);
-    };
-    requestAnimationFrame(animate);
+    this.walkInterval = setInterval(() => {
+      if (!this.isDead) {
+        this.playAnimation(this.IMAGES_WALKING);
+      } else {
+        clearInterval(this.walkInterval);
+      }
+    }, 100);
+  }
+
+  endbossAlertAnimation() {
+    this.alertInterval = setInterval(() => {
+      if (!this.isDead) {
+        this.playAnimation(this.IMAGES_ALERT);
+      } else {
+        clearInterval(this.alertInterval);
+      }
+    }, 100);
+  }
+
+  endbossAttackAnimation() {
+    this.attackInterval = setInterval(() => {
+      if (!this.isDead) {
+        this.playAnimation(this.IMAGES_ATTACK);
+      } else {
+        clearInterval(this.attackInterval);
+      }
+    }, 100);
+  }
+
+  endbossHurtAnimation() {
+    this.hurtInterval = setInterval(() => {
+      if (!this.isDead) {
+        this.playAnimation(this.IMAGES_HURT);
+      } else {
+        clearInterval(this.hurtInterval);
+      }
+    }, 100);
+  }
+
+  // TODO World.class.js:108 Uncaught TypeError: enemy.die is not a function
+  endbossDeadAnimation() {
+    this.deadInterval = setInterval(() => {
+      if (this.isDead) {
+        this.playAnimation(this.IMAGES_DEAD);
+      } else {
+        clearInterval(this.deadInterval);
+      }
+    }, 100);
   }
 }
