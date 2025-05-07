f21bfa8d4877df960e70507a0a8b2f8eff2e1693
Anne Dalchow <anne.dalchow@web.de>
2025-05-07 16:07:12 +0200

    Update SoundManager.class.js

--- modules/SoundManager.class.js
+++ modules/SoundManager.class.js
@@ -1,18 +1,52 @@
 export default class SoundManager {
   constructor() {
-    this.enabled = true;
+    this.sounds = {};
+    this.muted = false;
   }
 
-  toggle(state) {
-    this.enabled = state;
+  play(path, volume = 1, loop = false, playbackRate = 1) {
+    try {
+      let sound;
+
+      if (this.sounds[path]) {
+        sound = this.sounds[path];
+      } else {
+        sound = new Audio(path);
+        sound.dataset.originalVolume = volume;
+        sound.dataset.originalPlaybackRate = playbackRate;
+        this.sounds[path] = sound;
+      }
+
+      sound.loop = loop;
+      sound.volume = this.muted ? 0 : volume;
+      sound.playbackRate = playbackRate;
+      sound.play();
+      return sound;
+    } catch (e) {
+      console.warn(`Sound konnte nicht abgespielt werden: ${path}`, e);
+    }
   }
 
-  play(filePath, volume = 1, playbackRate = 1) {
-    if (!this.enabled) return;
-    const audio = new Audio(filePath);
-    audio.volume = volume;
-    audio.playbackRate = playbackRate;
-    audio.play();
-    return audio;
+  unmuteAll() {
+    this.muted = false;
+    Object.values(this.sounds).forEach((sound) => {
+      const originalVolume = parseFloat(sound.dataset.originalVolume || 1);
+      const originalPlaybackRate = parseFloat(
+        sound.dataset.originalPlaybackRate || 1
+      );
+      sound.volume = originalVolume;
+      sound.playbackRate = originalPlaybackRate;
+    });
+  }
+
+  muteAll() {
+    this.muted = true;
+    Object.values(this.sounds).forEach((sound) => {
+      sound.volume = 0;
+    });
+  }
+
+  toggleMute() {
+    this.muted ? this.unmuteAll() : this.muteAll();
   }
 }
