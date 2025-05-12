a452a7f389500d8ed95252b5f2dc4df51bfaaceb
Anne Dalchow <anne.dalchow@web.de>
2025-05-12 20:28:10 +0200

    feat(controls): add and implement touch controls

--- modules/SoundManager.class.js
+++ modules/SoundManager.class.js
@@ -5,44 +5,30 @@ export default class SoundManager {
   }
 
   prepare(path, volume = 1, loop = false, playbackRate = 1) {
-    if (!this.sounds[path]) {
-      const sound = new Audio(path);
-      sound.loop = loop;
-      sound.volume = this.muted ? 0 : volume;
-      sound.playbackRate = playbackRate;
-      sound.dataset.originalVolume = volume;
-      sound.dataset.originalPlaybackRate = playbackRate;
-      this.sounds[path] = sound;
-    }
+    let sound = new Audio(path);
+    sound.loop = loop;
+    sound.volume = this.muted ? 0 : volume;
+    sound.playbackRate = playbackRate;
+    sound.dataset.originalVolume = volume;
+    sound.dataset.originalPlaybackRate = playbackRate;
+    this.sounds[path] = sound;
+
     return this.sounds[path];
   }
 
   play(path) {
-    try {
-      const sound = this.sounds[path];
-      if (!sound) {
-        console.warn(`Sound wurde nicht vorbereitet: ${path}`);
-        return;
-      }
-      sound.play();
-      return sound;
-    } catch (e) {
-      console.warn(`Sound konnte nicht abgespielt werden: ${path}`, e);
-    }
+    return this.sounds[path].play();
   }
 
   pause(path) {
-    const sound = this.sounds[path];
-    if (sound && !sound.paused) {
-      sound.pause();
-    }
+    return this.sounds[path].pause();
   }
 
   unmuteAll() {
     this.muted = false;
     Object.values(this.sounds).forEach((sound) => {
-      const originalVolume = parseFloat(sound.dataset.originalVolume || 1);
-      const originalPlaybackRate = parseFloat(
+      let originalVolume = parseFloat(sound.dataset.originalVolume || 1);
+      let originalPlaybackRate = parseFloat(
         sound.dataset.originalPlaybackRate || 1
       );
       sound.volume = originalVolume;
@@ -55,10 +41,6 @@ export default class SoundManager {
     Object.values(this.sounds).forEach((sound) => {
       sound.volume = 0;
     });
-  }
-
-  toggleMute() {
-    this.muted ? this.unmuteAll() : this.muteAll();
   }
 
   stopAndResetAllSounds() {
